92b009dbbcbf77ef52765a06e09394b4993b7100
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-10 17:59:00 +0530

    6th commit

---
+++ src/OddNumber/OddNumber1.java
@@ -0,0 +1,11 @@
+package OddNumber;
+
+public class OddNumber1 {
+    public static void main(String[] args){
+        for(int j =1;j<=20;j++){
+            if(j%2==1){
+                System.out.println(j);
+            }
+        }
+    }
+}
