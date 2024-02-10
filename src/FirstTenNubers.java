6901934fab95984b921dc31417985aac94ecc1c0
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-10 13:08:30 +0530

    3rd commit

---
+++ src/FirstTenNubers.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+class FirstTenNumbers {
+    public static void main(String[] args){
+        for(int i = 1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+
+}
