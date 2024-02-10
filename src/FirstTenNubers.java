46feaa0abf2c56732188c5db140686495ee5aae8
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-10 16:22:33 +0530

    4th commit

--- src/FirstTenNubers.java
+++ src/FirstTenNubers.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 class FirstTenNumbers {
     public static void main(String[] args){
-        for(int i = 1;i<=10;i++){
+        for(int i = 9;i<=11;i++){
             System.out.println(i);
         }
     }
