fb90fef1100958de4532f1992cf782b292eb9d2d
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-11 09:31:24 +0530

    8th commit

--- src/forlloopprograms/Pyramid.java
+++ src/forlloopprograms/Pyramid.java
@@ -1,13 +1,2 @@
 package forlloopprograms;
 
-public class Pyramid {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 4; j>= i; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-}
---
+++ src/forlloopprograms/Pyramid1.java
@@ -0,0 +1,14 @@
+package forlloopprograms;
+
+public class Pyramid1 {
+    public static void main(String[] args){
+        int i,j;
+
+        for(i=1;i<=4;i++){
+            for(j=1;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
