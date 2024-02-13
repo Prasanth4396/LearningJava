7b74e75f2ff5a2c1b90d6cba72d6198be60af755
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-13 08:13:40 +0530

    14th

---
+++ src/forlloopprograms/WhileSoft.java
@@ -0,0 +1,13 @@
+package forlloopprograms;
+
+public class WhileSoft {
+    public static void main(String[] args){
+        int i =1;
+        while(i<=5){
+            System.out.println("Prasanth");
+            i++;
+        }
+    }
+}
+
+
