23341b918640e6f6baaffc90e7a65af75b22d252
yanneriprasanth <yanneriprasanth@gmail.com>
2024-02-21 22:59:30 +0530

    15

--- src/ArraysLearning.java
+++ src/ArraysLearning.java
@@ -1,6 +1,6 @@
 public class ArraysLearning {
     public static void main(String[] args){
-        int[] a=new int[5];
+        int[] a =new int[5];
         a[0]=10;
         a[1]=20;
         a[2]=50;
---
+++ src/forlloopprograms/Student.java
@@ -0,0 +1,12 @@
+package forlloopprograms;
+
+public class Student {
+    public static void main(String[] args){
+        int[] marks ={50,60,70,80,90};
+        int i;
+        for(i=0;i< marks.length;i++){
+            System.out.println(marks[i]+ " ");
+        }
+        }
+        }
+
