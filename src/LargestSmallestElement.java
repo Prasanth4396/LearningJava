7c5421b7ded90dbcd4015642f368c0d68d2f710d
yanneriprasanth <yanneriprasanth@gmail.com>
2024-03-04 20:42:18 +0530

    test3

---
+++ src/LargestSmallestElement.java
@@ -0,0 +1,14 @@
+public class LargestSmallestElement {
+    public static void main(String[] args) {
+        int[] numbers = {10, 30, 70, 40, 100};
+        int largest = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+               largest=numbers[i];
+            }
+
+        }
+        System.out.print("largest number is "+ largest);
+    }
+
+}
