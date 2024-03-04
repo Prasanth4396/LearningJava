e3d24b5a0f331a583481411a1d9960881064c3f0
yanneriprasanth <yanneriprasanth@gmail.com>
2024-03-04 10:26:40 +0530

    Test

--- src/ArrayReverse.java
+++ src/ArrayReverse.java
@@ -1,11 +1,11 @@
 public class ArrayReverse {
     public static void main(String[] args){
 
-        int[] numbers ={12,2,3,8,9};
+        int[] numbers ={123,24,39,87,97};
         int i;
         for(i= numbers.length-1;i>=0;i--){
+
             System.out.print(numbers[i]+" ");
         }
 }
-
 }
--- src/ArraysLearning.java
+++ src/ArraysLearning.java
@@ -1,13 +1,14 @@
 public class ArraysLearning {
-    public static void main(String[] args){
-        int[] a =new int[5];
-        a[0]=10;
-        a[1]=20;
-        a[2]=50;
-        a[3]=90;
-        a[4]=100;
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 50;
+        a[3] = 90;
+        a[4] = 100;
+        for (int i = 0;i <= a.length; i++){
+            System.out.print(a[i] + " ");
 
-        System.out.println(a[2]);
-
+        }
     }
 }
