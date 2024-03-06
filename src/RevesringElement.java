0cc8d33231616601300af472d6d9857310ce66f2
yanneriprasanth <yanneriprasanth@gmail.com>
2024-03-06 08:12:31 +0530

    test5

--- src/RevesringElement.java
+++ src/RevesringElement.java
@@ -1,4 +1,4 @@
-public class TestArray2 {
+public class RevesringElement {
     public static void main(String[] args){
         int[] x={22,33,44,55};
         for(int i= x.length-1;i>=0;i--){
--- src/SpecificElement.java
+++ src/SpecificElement.java
@@ -12,5 +12,6 @@ public class SpecificElement {
                 break;
             }
         }
+        System.out.println("Element is not found in the Array ");
     }
 }
