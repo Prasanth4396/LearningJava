62a291b51e9534153c8f08c8032cfba530a85fc5
yanneriprasanth <yanneriprasanth@gmail.com>
2024-03-08 10:18:42 +0530

    test5

---
+++ src/SortingAcendingElement.java
@@ -0,0 +1,21 @@
+public class SortingAcendingElement {
+    public static void main(String[] args) {
+        int[] numbers = {44, 88, 22, 77, 99};
+        int temp;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+
+        }
+        for (int a : numbers) {
+            System.out.print(a + " ");
+        }
+    }
+}
+
